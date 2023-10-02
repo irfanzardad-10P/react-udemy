cac860476d6e1182a6329bd033b2fbd19010219e
irfanzardad10P <irfan.zardad@10pearls.com>
2023-10-02 11:19:19 +0500

    lecture 65 end of section 4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import Expenses from "./components/Expenses";
+import Header from './components/header/Header';
+import Concept from './components/Concept/Concept';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
   return (
-   <Expenses expenses={expenses}></Expenses>
+    <div>
+      <Header />
+      <ul id="concepts">
+       <Concept concept={concepts[0]} />
+       <Concept concept={concepts[1]} />
+       <Concept concept={concepts[2]} />
+        
+      </ul>
+    </div>
   );
 }
 
